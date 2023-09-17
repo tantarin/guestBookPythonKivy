c10936e93300a0d05c22657bf492e0c4027287d5
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2023-09-17 14:46:47 +0300

    edit

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.scrollview import ScrollView
-
-from sqlalchemy import create_engine, Column, Integer, String, Text
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from kivy.core.window import Window
 
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
@@ -20,6 +10,9 @@ from kivy.uix.button import Button
 from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+Window.size = (450, 400)
+Window.clearcolor = (255 / 255, 186 / 255, 3 / 255, 1)
 
 Base = declarative_base()
 
@@ -35,7 +28,7 @@ class GuestEntry(Base):
 
 class GuestBookApp(App):
     def build(self):
-        self.engine = create_engine('sqlite:///:memory:')  # Используем SQLite в памяти
+        self.engine = create_engine('sqlite:///:memory:', echo=True)  # Используем SQLite в памяти
         Base.metadata.create_all(self.engine)
         Session = sessionmaker(bind=self.engine)
         self.session = Session()
@@ -65,20 +58,23 @@ class GuestBookApp(App):
         email = self.email_input.text
         comment = self.comment_input.text
         if name and comment:
+            # добавляем в бд
             new_entry = GuestEntry(name=name, email=email, comment=comment)  # Сохраняем email
             self.session.add(new_entry)
             self.session.commit()
+            # Очищаем поля
             self.name_input.text = ''
-            self.email_input.text = ''  # Очищаем поле для email
+            self.email_input.text = ''
             self.comment_input.text = ''
             self.display_entries()
 
     def display_entries(self):
         entries = self.session.query(GuestEntry).all()
+        print(entries)
         entry_text = ''
         for entry in entries:
             entry_text += f'Имя: {entry.name}\nEmail: {entry.email}\nКомментарий: {entry.comment}\n\n'
-        self.entry_display.text = entry_text
+            print(entry_text)
 
 
 if __name__ == '__main__':
